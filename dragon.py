3f15ef176498aa16db41644701416e99f698ed3e
Snowwolftriumph <124800209+Snowwolftriumph@users.noreply.github.com>
2023-02-08 07:54:11 -0700

    Create dragon.py

---
+++ dragon.py
@@ -0,0 +1,23 @@
+import turtle
+t = turtle.()
+square=1
+# makes the head
+while square <4:
+    t.forward(100)
+    t.right(90)
+    square=square +1
+# makes the body
+regtangle=1
+while rectangle <4:
+t.forward(100)
+t.left(90)
+t.forward(100)
+rectangle=rectangle+1
+# makes the tail  
+t.forward(100)  
+while 
+t.left(45)
+t.forward(100)
+t.left(90)
+t.forward
+# makes the legs
