3232b03224c789d626e96793b5f37a1c7d455d59
Vangelis007 <124800194+Vangelis007@users.noreply.github.com>
2023-02-14 08:31:18 -0700

    Update draw a dragon.py

--- draw a dragon.py
+++ draw a dragon.py
@@ -1,22 +1,99 @@
 import turtle
-t = turtle.()
-square=1
+t = turtle.Turtle()
+s = turtle.Screen()
+
 # makes the head
+square=1
+t.color("black", "red")
+t.begin_fill()
 while square <4:
-    t.forward(100)
-    t.right(90)
-    square=square +1
-# makes the body
-regtangle=1
-while rectangle <4:
+    
     t.forward(100)
     t.left(90)
-    t.forward(100)
-rectangle=rectangle+1
-# makes the tail
-t.forward(100)  
-t.left(45)
+    
+    square=square +1
+t.end_fill()
+#eyes
+t.color("black", "white")
+t.begin_fill()
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(40)
+t.left(180)
+t.forward(40)
+t.left(90)
+t.forward(40)
+t.left(90)
+t.forward(40)
+t.left(90)
+t.forward(80)
+t.left(90)
+t.forward(40)
+t.right(90)
+t.forward(10)
+t.right(90)
+t.forward(50)
+t.left(180)
+t.end_fill()
+#body
+t.color("black", "red")
+t.begin_fill()
+t.forward(200)
+t.left(90)
+t.forward(350)
+t.left(90)
 t.forward(100)
 t.left(90)
-t.forward
+t.forward(350)
+t.end_fill()    
+
+
+# makes the tail
+t.color("black", "red")
+t.begin_fill()
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(350)
+t.left(50)
+t.forward(150)
+t.left(90)
+t.forward(25)
+t.left(90)
+t.forward(125)
+t.right(50)
+t.forward(347)
+t.left(90)
+t.forward(40)
+t.end_fill()
+
+
 # makes the legs
+t.color("black", "red")
+t.begin_fill()
+t.forward(100)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(105)
+t.end_fill()
+
+
+t.color("black", "red")
+t.begin_fill()
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(105)
+t.end_fill()
+
+
+ 
+
+s.exitonclick()
